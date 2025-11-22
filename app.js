8d9e1112440e894bd7460cd54e368e5652cb8314
rbhavesh1525 <129406732+rbhavesh1525@users.noreply.github.com>
2025-11-22 14:52:07 +0530

    database and firebase setup successfully

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-// import userRoutes from "./routes/userRoutes.js";
 
+
+import { connectDB } from "./dbconfig/dbconfig.js";
 dotenv.config();
 
 const app = express();
@@ -17,7 +18,7 @@ app.get("/", (req, res) => {
 
 // app.use("/api/users", userRoutes);
 const PORT = process.env.PORT || 5000;
-
+connectDB();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${process.env.PORT || 5000}`);
 });
